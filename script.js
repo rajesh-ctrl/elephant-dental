ff09a689a9949a77da80fd6ec9e71ee004a81a07
rajesh-ctrl <a.rajeshkumar95@gmail.com>
2024-12-31 22:04:38 +0530

    31_24_last

--- script.js
+++ script.js
@@ -105,3 +105,20 @@ document.addEventListener('DOMContentLoaded', function () {
     // Automatically show the next card every 3 seconds
     setInterval(showNextCard, 3000);
 });
+
+
+
+// form
+document.addEventListener('DOMContentLoaded', function () {
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbx_a1uD1rFIcdBUcfc6C5bzxdpGvl7OtcF0AnLtFLi6_ZMIXU3dMKDBV-01FsO1VJIafw/exec'
+
+    const form = document.forms['contact-form']
+
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+            .then(response => alert("Thank you! your form is submitted successfully."))
+            .then(() => { window.location.reload(); })
+            .catch(error => console.error('Error!', error.message))
+    })
+});
