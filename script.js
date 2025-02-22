9355fade4a8641da77a9fa6f5ec8129f3d072154
rajesh-ctrl <a.rajeshkumar95@gmail.com>
2025-02-22 13:14:41 +0530

    250222

--- script.js
+++ script.js
@@ -109,16 +109,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
 // form
-document.addEventListener('DOMContentLoaded', function () {
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbx_a1uD1rFIcdBUcfc6C5bzxdpGvl7OtcF0AnLtFLi6_ZMIXU3dMKDBV-01FsO1VJIafw/exec'
+// document.addEventListener('DOMContentLoaded', function () {
+//     const scriptURL = 'https://script.google.com/macros/s/AKfycbx_a1uD1rFIcdBUcfc6C5bzxdpGvl7OtcF0AnLtFLi6_ZMIXU3dMKDBV-01FsO1VJIafw/exec'
 
-    const form = document.forms['contact-form']
+//     const form = document.forms['contact-form']
 
-    form.addEventListener('submit', e => {
-        e.preventDefault()
-        fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-            .then(response => alert("Thank you! your form is submitted successfully."))
-            .then(() => { window.location.reload(); })
-            .catch(error => console.error('Error!', error.message))
-    })
-});
+//     form.addEventListener('submit', e => {
+//         e.preventDefault()
+//         fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+//             .then(response => alert("Thank you! your form is submitted successfully."))
+//             .then(() => { window.location.reload(); })
+//             .catch(error => console.error('Error!', error.message))
+//     })
+// });
